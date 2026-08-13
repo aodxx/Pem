7ff2bdfa1b272bc77a89de79b564fd5bbd92061e
aodxx <aodaod3826@gmail.com>
2026-08-13 17:21:49 +1200

    fix(reliability): update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PALM_CONFIG = Object.freeze({
   apiUrl: 'https://script.google.com/macros/s/AKfycbwttI8iFWVls788jXX-nV_7MZsFvwGkwDaIU3JdfcmEqH9zYYzQ5pxGeSza6NLJqmxQGA/exec',
-  version: '1.0.0'
+  version: '1.1.0'
 });
