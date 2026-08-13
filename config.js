2506a7d6b0897853568565dca7513fbfb7b72a5a
aodxx <aodaod3826@gmail.com>
2026-08-14 05:25:07 +1200

    Add in-app receipt image viewer
    Let users view the original weighing receipt inside PALM LEDGER and keep editing as a separate explicit action.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.PALM_CONFIG = Object.freeze({
   apiUrl: 'https://script.google.com/macros/s/AKfycbwttI8iFWVls788jXX-nV_7MZsFvwGkwDaIU3JdfcmEqH9zYYzQ5pxGeSza6NLJqmxQGA/exec',
-  version: '2.0.1'
+  version: '2.1.0'
 });
