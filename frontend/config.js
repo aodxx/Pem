744535a5f81f306f90cc4ce9d2a6fe2f7f9bb5f7
aodxx <aodaod3826@gmail.com>
2026-08-15 18:19:38 +1200

    Align app version after Pages restore

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 window.PALM_CONFIG = Object.freeze({
   apiUrl: 'https://script.google.com/macros/s/AKfycbwttI8iFWVls788jXX-nV_7MZsFvwGkwDaIU3JdfcmEqH9zYYzQ5pxGeSza6NLJqmxQGA/exec',
-  version: '2.5.1'
+  version: '2.5.2'
 });
